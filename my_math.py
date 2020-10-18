cca466179560c3fd2957aed4f3268f58e8ec908f
Jason <70834549+Jason100129@users.noreply.github.com>
2020-10-18 19:56:54 +0800

    Update my_math.py

--- my_math.py
+++ my_math.py
@@ -1,3 +1,4 @@
+e = 2.718281828459045
 class my_math:
     pi = 3.141592653589793
     e = 2.718281828459045
@@ -10,7 +11,11 @@ class my_math:
     def floor(self, x):
         x = x//1
         return int(x)
-        
+    def pow(self, x, y):
+        return x**y
+    def exp(self, x):
+        return e*x
+         
 math = my_math()
 
 #未完成
